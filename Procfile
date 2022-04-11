373584b79472b0cbf6cc89eed204a6ea13be3941
Sanjay Gupta <guptasanjay9091@gmail.com>
2022-04-11 20:43:14 +0530

    Heroku config changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn:TODOlist.wsgi --log-file -
--- TODOlist/settings.py
+++ TODOlist/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2el@wb2vs=p+%%(aqo_t+20odsl=5uhu6xdubemp4@e8!#b(7c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taskfortheday.herokuapp.com','127.0.0.1']
 
 
 # Application definition
